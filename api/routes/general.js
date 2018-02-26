3ee6ea73d6e5b09ed3a96a2f02b7a6110d5d1667
anusree <anusree@alignminds.com>
2018-02-26 18:21:30 +0530

    video stream

--- api/routes/general.js
+++ api/routes/general.js
@@ -12,7 +12,10 @@ router.get('/video',(req, res, next) => {
             const path =  __dirname+'/SampleVideo.mp4'
             const stat = fs.statSync(path)
             const fileSize = stat.size
+            //const range = req.headers.range ? req.headers.range: "bytes=0-10000";
             const range = req.headers.range
+
+
             if (range) {
               const parts = range.replace(/bytes=/, "").split("-")
               const start = parseInt(parts[0], 10)
@@ -29,7 +32,7 @@ router.get('/video',(req, res, next) => {
               }
               res.writeHead(206, head);
               file.pipe(res);
-            } else {
+            } else {console.log('Else part');
               const head = {
                 'Content-Length': fileSize,
                 'Content-Type': 'video/mp4',
